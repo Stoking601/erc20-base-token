8c59f2efc43f33e4766ee184365f148dc56c221e
Stoking601 <stoking001@gmail.com>
2026-06-29 14:02:42 +0700

    feat: add Pausable and Minted event to MyToken

--- src/MyToken.sol
+++ src/MyToken.sol
@@ -3,28 +3,29 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-/// @title MyToken
-/// @notice ERC20 token deployed on Base
-/// @dev Includes burn and mint functionality
-contract MyToken is ERC20, ERC20Burnable, Ownable {
-    uint256 public constant MAX_SUPPLY = 1_000_000_000 * 10 ** 18; // 1 billion tokens
+contract MyToken is ERC20, ERC20Burnable, ERC20Pausable, Ownable {
+    uint256 public constant MAX_SUPPLY = 1_000_000_000 * 10 ** 18;
+    event Minted(address indexed to, uint256 amount);
 
-    constructor(
-        string memory name,
-        string memory symbol,
-        address initialOwner
-    ) ERC20(name, symbol) Ownable(initialOwner) {
-        // Mint 10% of max supply to deployer
+    constructor(string memory name, string memory symbol, address initialOwner)
+        ERC20(name, symbol) Ownable(initialOwner) {
         _mint(initialOwner, MAX_SUPPLY / 10);
     }
 
-    /// @notice Mint new tokens (only owner)
-    /// @param to Recipient address
-    /// @param amount Amount to mint
     function mint(address to, uint256 amount) external onlyOwner {
         require(totalSupply() + amount <= MAX_SUPPLY, "Exceeds max supply");
         _mint(to, amount);
+        emit Minted(to, amount);
+    }
+
+    function pause() external onlyOwner { _pause(); }
+    function unpause() external onlyOwner { _unpause(); }
+
+    function _update(address from, address to, uint256 value)
+        internal override(ERC20, ERC20Pausable) {
+        super._update(from, to, value);
     }
 }
